69199a2d1e538b4c669eb98814474faa00904066
vitorbina <vitor.bina10@gmail.com>
2026-02-05 16:47:43 -0300

    setup ingestion pipeline, update requirements and organize project structure

--- dags/ingestion.py
+++ dags/ingestion.py
@@ -1,6 +1,6 @@
 import os
 from airflow import DAG
-from airflow.operators.bash import BashOperator
+from airflow.providers.standard.operators.bash import BashOperator
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
@@ -17,22 +17,16 @@ default_args = {
 
 with DAG(
     dag_id= 'nyc_taxi_ingestion',
-    default_args=default_args,
     start_date=datetime(2020,1,1),
-    end_date= cutoff_date,
-    schedule_interval='@monthly',
-    description='Monthly ingestion for NYC Taxi Data',
-    catchup=True,
-    max_active_runs=3,
+    schedule='@monthly',
+    catchup=False,
     tags = ['nyc_taxi', 'ingestion'],
 ) as dag:
     ingest_task = BashOperator(
         task_id='ingest_monthly_data',
-        bash_command=f"""
-        source ~/miniconda3/etc/profile.d/conda.sh && \
-        conda activate projeto-dados && \
-        python {PROJECT_PATH}/scripts/ingestion_script.py \
-        --year {{ logical_date.strftime('%Y') }} \
-        --month {{ logical_date.strftime('%m') }}
-        """
-    )
+bash_command="""source ~/miniconda3/etc/profile.d/conda.sh && \
+conda activate projeto-dados && \
+python /home/vitor/projects/nyc-taxi/scripts/ingestion_script.py \
+--year {{ ds_nodash[:4] }} \
+--month {{ ds_nodash[4:6] }}"""
+)
